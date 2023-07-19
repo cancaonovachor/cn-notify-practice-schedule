05b1abd5856a7279a29be7ffa0ad1d947526dfc9
Kenshiro <kenshiro.toptenor@gmail.com>
2023-07-19 22:05:46 +0900

    練習時間も表示

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import googleapiclient.discovery
 import google.auth
 import tweepy
 from local_settings import *
+import unicodedata
 
 # Google APIの準備をする
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -57,10 +58,11 @@ def get_events_in_month(year, month):
         for event in kansai_events:
             if re.match(r'^\d{4}-\d{2}-\d{2}$', event[0]):
                 start_date = datetime.datetime.strptime(event[1], '%Y-%m-%d')
-                response += f'{start_date.strftime("%#m/%#d")} @{extract_text_in_parentheses(event[2])}\n'
+                response += f'{start_date.strftime("%#m/%#d")} @{extract_text_in_parentheses(event[2])} ({start_date.strftime("%H:%M")})\n'
             else:
                 start_time = datetime.datetime.strptime(event[0], '%Y-%m-%dT%H:%M:%S+09:00')
-                response += f'{start_time.strftime("%#m/%#d")} @{extract_text_in_parentheses(event[2])}\n'
+                end_time = datetime.datetime.strptime(event[1], '%Y-%m-%dT%H:%M:%S+09:00')
+                response += f'{start_time.strftime("%#m/%#d %#H:%M")}~{end_time.strftime("%#H:%M")} @{extract_text_in_parentheses(event[2])}\n'
         response += '\n'
 
     if kanto_events:
@@ -68,10 +70,12 @@ def get_events_in_month(year, month):
         for event in kanto_events:
             if re.match(r'^\d{4}-\d{2}-\d{2}$', event[0]):
                 start_date = datetime.datetime.strptime(event[1], '%Y-%m-%d')
-                response += f'{start_date.strftime("%#m/%#d")} @{extract_text_in_parentheses(event[2])}\n'
+                response += f'{start_date.strftime("%#m/%#d")} @{extract_text_in_parentheses(event[2])} ({start_date.strftime("%H:%M")})\n'
             else:
                 start_time = datetime.datetime.strptime(event[0], '%Y-%m-%dT%H:%M:%S+09:00')
-                response += f'{start_time.strftime("%#m/%#d")} @{extract_text_in_parentheses(event[2])}\n'
+                end_time = datetime.datetime.strptime(event[1], '%Y-%m-%dT%H:%M:%S+09:00')
+                print(end_time)
+                response += f'{start_time.strftime("%#m/%#d %#H:%M")}~{end_time.strftime("%#H:%M")} @{extract_text_in_parentheses(event[2])}\n'
         response += '\n'
 
     if other_events:
@@ -82,11 +86,13 @@ def get_events_in_month(year, month):
                 response += f'{start_date.strftime("%#m/%#d")} {event[2]}\n\n'
             else:
                 start_time = datetime.datetime.strptime(event[0], '%Y-%m-%dT%H:%M:%S+09:00')
+                end_time = datetime.datetime.strptime(event[1], '%Y-%m-%dT%H:%M:%S+09:00')
                 a = re.split("[(（]", event[2])
+                print(end_time)
                 if extract_text_in_parentheses(event[2]) == None:
-                    response += f'{start_time.strftime("%#m/%#d")} {a[0]}\n\n'
+                    response += f'{start_time.strftime("%#m/%#d %H:%M")}~{end_time.strftime("%#H:%M")} {a[0]}\n'
                 else:
-                    response += f'{start_time.strftime("%#m/%#d")} {a[0]} @{extract_text_in_parentheses(event[2])}\n\n'
+                    response += f'{start_time.strftime("%#m/%#d %#H:%M")}~{end_time.strftime("%#H:%M")} {a[0]} @{extract_text_in_parentheses(event[2])}\n'
         response += '\n'
 
     response = response.rstrip('\n')
@@ -102,7 +108,6 @@ list_rehearsal = get_events_in_month(year, month)
 tweet_text = f"こんにちは!CancaoNovaです!\n{month}月の練習日程は\n\n{list_rehearsal}\n\nとなっております!"
 
 print(tweet_text)
-print(len(tweet_text))
 
 # Tweepyの認証
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
@@ -116,5 +121,5 @@ def post_to_twitter(tweet):
     except tweepy.TweepError as e:
         print("投稿に失敗しました:", e.reason)
 
-# Twitterに投稿
-post_to_twitter(tweet_text)
+# # Twitterに投稿
+# post_to_twitter(tweet_text)
